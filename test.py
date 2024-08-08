7633d0583c5a5ac19b71ff0e760478dc73d3a78f
M-Aitisam <aitisamahmed24@gmail.com>
2024-08-08 20:39:26 +0500

    Add the file

---
+++ test.py
@@ -0,0 +1,67 @@
+import os
+
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def divide_numbers(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Division by zero is not allowed"
+
+def read_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content
+    else:
+        return "File not found"
+
+def write_to_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
+    return "Content written to file"
+
+class Person:
+    def __init__(self, name, age):
+        self
+
+import os
+
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def divide_numbers(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Division by zero is not allowed"
+
+def read_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content
+    else:
+        return "File not found"
+
+def write_to_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
+    return "Content written to file"
+
+class Person:
+    def __init__(self, name, age):
+        self
